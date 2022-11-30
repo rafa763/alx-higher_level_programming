b682dfb2a8967a7184bdc26c238b59956bd83b64
rafa763 <mohamed.raafat@eng.suez.edu.eg>
2022-11-30 10:24:51 +0000

    rev

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 for i in reversed(range(97, 123)):
-    print("{}".format(chr(i)), end="")
+    if i % 2 == 0:
+        print("{}".format(chr(i)), end="")
+    else:
+        print("{}".format(chr(i-32)), end="")
