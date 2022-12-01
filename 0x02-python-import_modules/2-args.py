57e7839b57b60baccbbf1f260ef0741ba725e55a
rafa763 <mohamed.raafat@eng.suez.edu.eg>
2022-12-01 21:28:08 +0000

    args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,6 +4,9 @@ if __name__ == "__main__":
     if len(argv) == 1:
         print("0 arguments.")
     else:
-        print("{} argument:".format(len(argv) - 1))
+        if len(argv) == 2:
+            print("{} argument:".format(len(argv) - 1))
+        else:
+            print("{} arguments:".format(len(argv) - 1))
         for i in range(1, len(argv)):
             print("{}: {}".format(i, argv[i]))
