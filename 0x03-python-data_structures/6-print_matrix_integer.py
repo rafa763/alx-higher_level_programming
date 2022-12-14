3bfd6c319d7461aab80ae1ebd48bc239c1efd4f8
rafa763 <mohamed.raafat@eng.suez.edu.eg>
2022-12-14 07:32:27 +0200

    print matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,7 +2,7 @@
 def print_matrix_integer(matrix=[[]]):
     for li in matrix:
         for j in range(len(li)):
-            print(li[j], end="")
+            print("{}".format(li[j]), end="")
             if j < len(li) - 1:
                 print(" ", end="")
         print()
