b09aabf3aec32de772ffcc60c03d64e8ea7bf2dd
rafa763 <mohamed.raafat@eng.suez.edu.eg>
2022-12-30 01:28:35 +0000

    unique

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 def uniq_add(my_list=[]):
-    l = list(set(my_list))
+    new_list = list(set(my_list))
     n = 0
-    for x in l:
+    for x in new_list:
         n += x
     return n
