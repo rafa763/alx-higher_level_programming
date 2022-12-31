361fda37314f7f26dcee8c6016824e80e6d5bf02
rafa763 <mohamed.raafat@eng.suez.edu.eg>
2022-12-31 19:23:37 +0000

    highest

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
 def best_score(a_dictionary):
-    if a_dictionary == None:
+    if a_dictionary is None or len(a_dictionary) == 0:
         return None
     return (list(sorted(a_dictionary.items()))[-1][0])
