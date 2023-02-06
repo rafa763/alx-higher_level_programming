3faa7bbdeeaf34991e50351f7010e2aad82a723f
rafa763 <mohamed.raafat@eng.suez.edu.eg>
2023-02-06 09:30:04 +0200

    class

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
+"""Empty class that defines a square"""
 class Square:
-    """Empty class that defines a square"""
     def __init__(self):
         """Initialize the class"""
         pass
