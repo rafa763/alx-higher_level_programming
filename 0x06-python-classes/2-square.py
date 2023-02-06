b8804635b3d507cd3b1486fe12d1ad8c60ef75b0
rafa763 <mohamed.raafat@eng.suez.edu.eg>
2023-02-06 10:25:02 +0200

    validate

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -13,7 +13,7 @@ class Square:
 
         if type(size) != int:
             raise TypeError("size must be an integer")
-        elif size< 0:
+        elif size < 0:
             raise ValueError("size must be >= 0")
         else:
             self.__size = size
