aa29252fc1c3c9ceae539808ac92fcdebdd793d0
rafa763 <mohamed.raafat@eng.suez.edu.eg>
2023-02-07 08:18:04 +0200

    __del__

--- 0x08-python-more_classes/5-rectangle.py
+++ 0x08-python-more_classes/5-rectangle.py
@@ -17,6 +17,7 @@ class Rectangle:
     def __repr__(self):
         return ("{}({}, {})".format(self.__class__.__name__,
                 self.__width, self.__height))
+
     def __del__(self):
         print("Bye rectangle...")
 
