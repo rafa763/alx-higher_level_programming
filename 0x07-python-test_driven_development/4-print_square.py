ebe0116ae051abc8a98adc3fddd773b93d01d227
rafa763 <mohamed.raafat@eng.suez.edu.eg>
2023-02-08 07:00:18 +0200

    print

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -12,7 +12,7 @@ def print_square(size):
         ValueError: if the size is less than zero
     """
     if type(size) != int:
-        raise TypeError("size must be an integer")    
+        raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
     if type(size) == float and size < 0:
