3eef01a294a635893ba544f3a77b0233905ea699
rafa763 <mohamed.raafat@eng.suez.edu.eg>
2023-02-08 05:36:46 +0200

    print

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""print passed name in a sentence"""
+
+
+def say_my_name(first_name, last_name=""):
+    """
+    print the passed first name and last name
+    Args:
+        param1 (first_name)
+        param2 (last_name)
+    Exception:
+        TypeError: if either first_name or last_name is not a string
+    """
+    if type(first_name) != str :
+        raise TypeError("first_name must be a string")
+    if type(last_name) != str:
+        raise TypeError("last_name must be a string")
+    print("My name is {} {}".format(first_name, last_name))
