df3c372671d8d0809183602c1ddd020a26b309fa
rafa763 <mohamed.raafat@eng.suez.edu.eg>
2023-02-09 14:22:04 +0200

    print

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -6,6 +6,3 @@ class MyList(list):
     """method to sort the list"""
     def print_sorted(self):
         print(sorted(self))
-
-    def __str__(self):
-        return "{} converts unsorted list into sorted list".format(type(self).__name__)
