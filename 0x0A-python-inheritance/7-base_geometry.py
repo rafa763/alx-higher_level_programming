0a9791d145c72ecf3df8226450dbc46b1a02d315
rafa763 <mohamed.raafat@eng.suez.edu.eg>
2023-02-09 16:37:02 +0200

    method

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""base class"""
+
+
+class BaseGeometry:
+    """
+    initialize the class
+    """
+    def __init(self, name, value):
+        self.name = name
+        self.value = value
+
+    """
+    validate the passed integer
+    Args:
+        param1 (name): the name of the passed integer
+        param2 (value): the value of the name
+    Exceptions:
+        TypeError: if value is not an integer
+        ValueError: if value is less than or equal to zero
+    """
+    def integer_validator(self, name, value):
+        if type(value) != int:
+            raise TypeError("<name> must be an integer")
+        if value <= 0:
+            raise ValueError("<name> must be greater than 0")
+
+    """method that raises exception"""
+    def area(self):
+        raise Exception("area() is not implemented")
