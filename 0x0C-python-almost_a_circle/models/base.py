a8f461f1fe9596b6b99356e12d2307e5457222e3
rafa763 <mohamed.raafat@eng.suez.edu.eg>
2023-02-13 13:45:19 +0200

    base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -6,6 +6,9 @@ class Base:
     __nb_objects = 0
     """
     Assign id value based on the passed argument
+    Args:
+        id: the id of the created instance
+        nb_objects: no of the created instances
     """
     def __init__(self, id=None):
 
