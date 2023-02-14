aab3b7e87f84cfa081b625fd4ed49100f4123bb9
rafa763 <mohamed.raafat@eng.suez.edu.eg>
2023-02-14 04:18:25 +0200

    area

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -20,110 +20,110 @@ class Rectangle(Base):
         self.x = x
         self.y = y
 
-    """
-    width getter method
-    Returns:
-        return value of width
-    """
     @property
     def width(self):
+        """
+        width getter method
+        Returns:
+            return value of width
+        """
         return self.__width
 
-    """
-    width setter method
-    Args:
-        width: value to assign for rectangle width
-    Exceptions:
-        TypeError: if width is not an integer
-        ValueError: if width is less than or equal to zero
-    """
     @width.setter
     def width(self, width):
+        """
+        width setter method
+        Args:
+            width: value to assign for rectangle width
+        Exceptions:
+            TypeError: if width is not an integer
+            ValueError: if width is less than or equal to zero
+        """
         if type(width) != int:
             raise TypeError("width must be an integer")
         if width <= 0:
             raise ValueError("width must be > 0")
         self.__width = width
 
-    """
-    height getter method
-    Return:
-        value of height
-    """
     @property
     def height(self):
+        """
+        height getter method
+        Return:
+            value of height
+        """
         return self.__height
 
-    """
-    height setter method
-    Args:
-        height: value to assign for rectangle height
-    Exceptions:
-        TypeError: if height is not an integer
-        ValueError: if height is less than or equal to zero
-    """
     @height.setter
     def height(self, height):
+        """
+        height setter method
+        Args:
+            height: value to assign for rectangle height
+        Exceptions:
+            TypeError: if height is not an integer
+            ValueError: if height is less than or equal to zero
+        """
         if type(height) != int:
             raise TypeError("height must be an integer")
         if height <= 0:
             raise ValueError("height must be > 0")
         self.__height = height
 
-    """
-    x getter method
-    Return:
-        value of x
-    """
     @property
     def x(self):
+        """
+        x getter method
+        Return:
+            value of x
+        """
         return self.__x
 
-    """
-    x setter method
-    Args:
-        x: value to assign for x
-    Exceptions:
-        TypeError: if x is not an integer
-        ValueError: if x is less than zero
-     """
     @x.setter
     def x(self, value):
+        """
+        x setter method
+        Args:
+            x: value to assign for x
+        Exceptions:
+            TypeError: if x is not an integer
+            ValueError: if x is less than zero
+         """
         if type(value) != int:
             raise TypeError("x must be an integer")
         if value < 0:
             raise ValueError("x must be >= 0")
         self.__x = value
 
-    """
-    y getter method
-    Return:
-        value of y
-    """
     @property
     def y(self):
+        """
+        y getter method
+        Return:
+            value of y
+        """
         return self.__y
 
-    """
-    y setter method
-    Args:
-        x: value to assign for x
-    Exceptions:
-        TypeError: if x is not an integer
-        ValueError: if x is less than zero
-    """
     @y.setter
     def y(self, value):
+        """
+        y setter method
+        Args:
+            x: value to assign for x
+        Exceptions:
+            TypeError: if x is not an integer
+            ValueError: if x is less than zero
+        """
         if type(value) != int:
             raise TypeError("y must be an integer")
         if value < 0:
             raise ValueError("y must be >= 0")
         self.__y = value
 
-    """
-    calclate area public method
-    Returns:
-        area of the rectangle (width * height)
-    """
     def area(self):
-        return self.__width * self.__height
+        """
+        calclate area public method
+        Returns:
+            area of the rectangle (width * height)
+        """
+        return self.width * self.height
