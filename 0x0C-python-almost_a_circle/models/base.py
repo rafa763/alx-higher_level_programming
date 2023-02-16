b69769c7ce5685c6aca4ebeaec41fc48b7aac4b9
rafa763 <mohamed.raafat@eng.suez.edu.eg>
2023-02-16 11:05:00 +0200

    JSON

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,6 +2,8 @@
 """create base class"""
 
 
+import json
+
 class Base:
     """
     Assign id value based on the passed argument
@@ -19,3 +21,10 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
