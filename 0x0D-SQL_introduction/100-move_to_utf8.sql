89ba6879b0382b251c1adfc3d04ee7e6f3c6a8c6
rafa763 <mohamed.raafat@eng.suez.edu.eg>
2023-02-18 13:53:38 +0200

    charset

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,3 +1,7 @@
 -- convert hbtn_0c_0 database to UTF8
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 ALTER TABLE first_table CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE first_table MODIFY 
+	name 
+	CHARACTER SET utf8mb4 
+	COLLATE utf8mb4_unicode_ci;
