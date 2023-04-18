c3ab7d060aa1c6e3f2d26feb8d4f35b3755f1edd
rafa763 <mohamed.raafat@eng.suez.edu.eg>
2023-04-18 07:53:06 +0200

    JSON body

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sends a JSON POST request to a URL passed as the first argument
-curl -X POST -d @$2 $1 -H 'content-type: application/json'
+curl -sX POST -d @$2 $1 -H 'content-type: application/json'
