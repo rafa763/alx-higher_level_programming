5b54a7a460cfb680dac27f97a56a756dec2b2d9c
rafa763 <mohamed.raafat@eng.suez.edu.eg>
2023-04-19 09:00:13 +0200

    fetch userId

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -11,10 +11,5 @@ if __name__ == "__main__":
 
     name = sys.argv[1]
     token = sys.argv[2]
-    headers = {
-            'Accept': 'application/vnd.github+json',
-            'Authorization': 'Bearer'+token,
-            'X-GitHub-Api-Version': '2022-11-28'
-            }
-    r = requests.get('https://api.github.com/users/'+name, headers)
+    r = requests.get('https://api.github.com/user', auth=(name, token))
     print(r.json().get('id'))
