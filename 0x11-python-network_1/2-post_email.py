538f8ac6503bf547fca50a1468de87d924ff1e15
rafa763 <mohamed.raafat@eng.suez.edu.eg>
2023-04-19 05:50:42 +0200

    post

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     import sys
 
     url = sys.argv[1]
-    email = { 'email': sys.argv[2]}
+    email = {'email': sys.argv[2]}
     data = urllib.parse.urlencode(email)
     data = data.encode('ascii')
     req = urllib.request.Request(url, data)
