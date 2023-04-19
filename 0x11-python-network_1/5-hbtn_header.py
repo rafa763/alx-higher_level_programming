46b89ad996015fbc647c32f5c89d0268dc689eef
rafa763 <mohamed.raafat@eng.suez.edu.eg>
2023-04-19 06:57:42 +0200

    headers

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+sends a request to the URL and displays
+the value of the X-Request-Id variable found
+in the header
+"""
+
+if __name__ == "__main__":
+    import requests
+    import sys
+
+    r = requests.get(sys.argv[1])
+    print(r.headers['X-Request-Id'])
