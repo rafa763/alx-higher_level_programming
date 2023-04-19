cf169a356cc8d19bb3051980d9b2e25f2912ec2b
rafa763 <mohamed.raafat@eng.suez.edu.eg>
2023-04-19 09:30:17 +0200

    query

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+xx
+"""
+
+if __name__ == '__main__':
+    import sys
+    import requests
+
+    name = sys.argv[1]
+    owner = sys.argv[2]
+
+    url = 'https://api.github.com/repos/{}/{}/commits'.format(name, owner)
+    r = requests.get(url)
+    r = r.json()
+    for i in range(10):
+        print('{}: {}'.format(
+            r[i].get('sha'),
+            r[i].get('commit').get('author').get('name'))
+            )
