da5b2a434a045510ffb4bef6cd9c1d7e4f5f5aa9
rafa763 <mohamed.raafat@eng.suez.edu.eg>
2023-04-19 09:35:33 +0200

    query

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -12,12 +12,12 @@ if __name__ == '__main__':
 
     url = 'https://api.github.com/repos/{}/{}/commits'.format(name, owner)
     r = requests.get(url)
-    r = r.json()
+    c = r.json()
     try:
         for i in range(10):
             print('{}: {}'.format(
-                r[i].get('sha'),
-                r[i].get('commit').get('author').get('name'))
+                c[i].get('sha'),
+                c[i].get('commit').get('author').get('name'))
                 )
     except IndexError:
         pass
