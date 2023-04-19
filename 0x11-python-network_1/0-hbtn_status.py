ae47f2fd0c97b1352215a5bf8187c56caf3f070c
rafa763 <mohamed.raafat@eng.suez.edu.eg>
2023-04-19 02:11:03 +0200

    fetch

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,9 +7,9 @@ using urllib backage
 if __name__ == "__main__":
     import urllib.request
 
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-       r = response.read()
-       print('Body response:')
-       print('\t- type: {}'.format(type(r)))
-       print('\t- content: {}'.format(r))
-       print('\t- utf8 content: {}'.format(r.decode('utf-8')))
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
+        r = res.read()
+        print('Body response:')
+        print('\t- type: {}'.format(type(r)))
+        print('\t- content: {}'.format(r))
+        print('\t- utf8 content: {}'.format(r.decode('utf-8')))
