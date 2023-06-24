555ba20baf4bb306f77aba1f5be642b454ebee9e
Mohamed Raafat <63686204+rafa763@users.noreply.github.com>
2023-06-24 03:26:14 +0300

    Delete 0x12-javascript-warm_up directory

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/node
-
-const arr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-
-arr.forEach((val) => {
-  console.log(val);
-});
--- 0x12-javascript-warm_up/9-add.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/node
-
-function add (a, b) {
-  console.log(Number(a) + Number(b));
-}
-
-add(process.argv[2], process.argv[3]);
